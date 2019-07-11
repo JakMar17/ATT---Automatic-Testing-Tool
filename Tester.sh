cc8bca9d81703aedd321558983d722e8e579996d
Jakob Marušič <jakob.marusic17@gmail.com>
2019-07-11 22:01:31 +0200

    Some of the code escaped from development to master, fixed

--- Tester.sh
+++ Tester.sh
@@ -132,10 +132,7 @@ function javaTesting()
         fileName=$(basename -- "$file")
 
         #running
-        start=$SECONDS
         timeout $timeout java $program < $file > $programOutput"/"$fileName
-        duration=$(( SECONDS - start ))
-        echo $duration
         exitStatus=$?
         #if timeout
         if [[ $exitStatus == 124 ]]; then
